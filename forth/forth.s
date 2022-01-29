2dd67d5f43db45dd893dc5e829c34d76df0f8fb7
Alexandre Dumont <adumont@gmail.com>
2022-01-29 16:59:35 +0100

    Forth: Shortcuts only available in BOOT mode
    Once we leave boot mode, I don't use shortcuts anymore as it makes it
    imposible to redefine  those words : ; , ...

--- forth/forth.s
+++ forth/forth.s
@@ -1415,6 +1415,10 @@ defword "FIND",,
 	LDA LATEST+1
 	STA W+1
 
+	lda BOOT			; Shortcuts are only available in BOOT mode	
+	beq @nxt_word		; after boot mode, I disable them. 
+						; shortcuts made it impossible to redefine those words
+
 ; shortcuts in FIND for ":" and ";"
 	LDA 2,X
 	CMP #1
