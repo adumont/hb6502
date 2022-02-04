c42ff8f9f4a22e5f2bfb39c8ce48fc8c3b85e56b
Alexandre Dumont <adumont@gmail.com>
2022-02-04 17:53:36 +0100

    6502 emulator: now accepts copy/pasted input

--- forth/py65forth.py
+++ forth/py65forth.py
@@ -2,6 +2,10 @@
 
 import sys
 import argparse
+import time
+import threading
+from queue import Queue
+import signal
 
 from py65.devices.mpu65c02 import MPU as CMOS65C02
 from py65.memory import ObservableMemory
@@ -16,69 +20,76 @@ args = parser.parse_args()
 getc_addr=0xF004
 putc_addr=0xF001
 
-def load(memory, start_address, bytes):
-    memory[start_address:start_address + len(bytes)] = bytes
+queue = Queue()
 
-def putc(address, value):
-    try:
-        sys.stdout.write(chr(value))
-    except UnicodeEncodeError: # Python 3
-        sys.stdout.write("?")
-    sys.stdout.flush()
+def signal_handler(signum, frame):
+    exit()
 
-def getc(address):
-    char = console.getch_noblock(sys.stdin)
-    if char:
-        byte = ord(char)
+signal.signal(signal.SIGINT, signal_handler)
+
+def cpuThread(ch, queue):
+    def load(memory, start_address, bytes):
+        memory[start_address:start_address + len(bytes)] = bytes
+
+    def putc(address, value):
+        try:
+            sys.stdout.write(chr(value))
+        except UnicodeEncodeError: # Python 3
+            sys.stdout.write("?")
+        sys.stdout.flush()
+
+    def getc(address):
+        if queue.empty():
+            return 0
+        else:
+            c=queue.get()
+            return c
+
+    def getByte(address):
+        return mpu.memory[address]
+
+    def getWord(address):
+        return mpu.memory[address] + 256*mpu.memory[address+1]
+
+    mpu = CMOS65C02()
+    mpu.memory = 0x10000 * [0xEA]
+
+    addrWidth = mpu.ADDR_WIDTH
+    # byteWidth = mpu.BYTE_WIDTH
+    # addrFmt = mpu.ADDR_FORMAT
+    # byteFmt = mpu.BYTE_FORMAT
+    # addrMask = mpu.addrMask
+    # byteMask = mpu.byteMask
+
+    m = ObservableMemory(subject=mpu.memory, addrWidth=addrWidth)
+    m.subscribe_to_write([putc_addr], putc)
+    m.subscribe_to_read([getc_addr], getc)
+    mpu.memory = m
+
+    if args.addr and str(args.addr).startswith("0x"):
+        args.addr = int(args.addr,16)
+
+    if args.rom:
+        print("Loading %s at $%04X" % ( args.rom, args.addr ) )
+        f = open(args.rom, 'rb')
+        program = f.read()
+        f.close()
     else:
-        byte = 0
-    return byte
+        # Dummy prog
+        program = [ 0xA9, 97, 0x8D, 0x01, 0xF0 ]
 
-def getByte(address):
-    return mpu.memory[address]
+    load(mpu.memory, args.addr, program)
 
-def getWord(address):
-    return mpu.memory[address] + 256*mpu.memory[address+1]
+    mpu.pc=getWord(mpu.RESET)
 
-mpu = CMOS65C02()
-mpu.memory = 0x10000 * [0xEA]
+    while True:
+        mpu.step()
 
-addrWidth = mpu.ADDR_WIDTH
-# byteWidth = mpu.BYTE_WIDTH
-# addrFmt = mpu.ADDR_FORMAT
-# byteFmt = mpu.BYTE_FORMAT
-# addrMask = mpu.addrMask
-# byteMask = mpu.byteMask
-
-m = ObservableMemory(subject=mpu.memory, addrWidth=addrWidth)
-m.subscribe_to_write([putc_addr], putc)
-m.subscribe_to_read([getc_addr], getc)
-mpu.memory = m
-
-if args.addr and str(args.addr).startswith("0x"):
-    args.addr = int(args.addr,16)
-
-if args.rom:
-    print("Loading %s at $%04X" % ( args.rom, args.addr ) )
-    f = open(args.rom, 'rb')
-    program = f.read()
-    f.close()
-else:
-    # Dummy prog
-    program = [ 0xA9, 97, 0x8D, 0x01, 0xF0 ]
-
-load(mpu.memory, args.addr, program)
-
-print(args)
-
-# print( "RESET:", "$%04X" % (mpu.RESET))
-
-# print( "RESET:", "$%04X" % getWord(mpu.RESET))
-# print( "IRQ  :", "$%04X" % getWord(mpu.IRQ))
-# print( "NMI  :", "$%04X" % getWord(mpu.NMI))
-
-# Reset: RESET vector => PC
-mpu.pc=getWord(mpu.RESET)
+t=threading.Thread( target=cpuThread, args=("", queue))
+t.daemon = True
+t.start()
 
 while True:
-    mpu.step()
+    char = console.getch(sys.stdin)
+    if char:
+        queue.put( ord(char) )
