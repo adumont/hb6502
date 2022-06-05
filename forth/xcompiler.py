cc5264142e6467b456349dcbd99825fb295d45ab
Alexandre Dumont <adumont@gmail.com>
2022-06-05 14:43:27 +0200

    xcompiler: compilation error handling + FIND offloading

--- forth/xcompiler.py
+++ forth/xcompiler.py
@@ -16,6 +16,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-r','--rom', help='binary rom file', default="forth.bin")
 parser.add_argument('-a','--addr', help='address to load to', default=0x8000)
 parser.add_argument('-l','--load', help='forth program to load')
+parser.add_argument('-f','--dfo', help='disable FIND offloading', default=False, action='store_true')
+parser.add_argument('-s','--symbols', help='symbols file', default="forth.lbl")
 args = parser.parse_args()
 
 getc_addr=0xF004
@@ -38,30 +40,70 @@ def signal_handler(signum, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# CPU_EXIT_SIGNALS
+CPU_EXIT_OK = 0
+CPU_EXIT_ERROR = 1
+
+symbols = None
+last_lookedup_word = ""
+
+def parseSymbolsFile(filename):
+    # file content should look like this:
+    # al 00C23D .do_PUSH1
+    # al 00C239 .__word_12
+    # al 00C239 .h_PUSH1
+    # al 00C22E .__word_11
+    symbols = {}
+    with open(filename) as file:
+        for line in file:
+            _, a, s = line.split(" ")
+            if s.startswith(".__word_") or s.startswith(".h_"):
+                # we discard those symbols
+                continue
+            symbols[int(a,16)]=s.strip()[1:]
+    return symbols
+
+def getSymbol(addr):
+    return symbols[max(k for k in symbols if k <= addr)]
+
+def getLabelAddr(label):
+    return [ k for k in symbols if symbols[k] == label ][0]
+
+if args.symbols:
+    symbols=parseSymbolsFile(args.symbols)
+
+# Address of symbols
+addr_NEXT = getLabelAddr("NEXT")
+addr_IP = 0xFE-2
+addr_LATEST = getLabelAddr("LATEST")
+addr_do_FIND = getLabelAddr("do_FIND")
+addr_do_0BR = getLabelAddr("do_0BR")
+addr_numberError = getLabelAddr("numberError")
+
 def cpuThread(ch, queue, emu_queue):
     started = False
 
     def load(memory, start_address, bytes):
         memory[start_address:start_address + len(bytes)] = bytes
 
-    def putc(address, value):
-        if not started:
-            return
-        try:
-            if value==0x08:
-                sys.stdout.write(chr(value))
-                sys.stdout.write(' ')
-            sys.stdout.write(chr(value))
-        except UnicodeEncodeError: # Python 3
-            sys.stdout.write("?")
-        sys.stdout.flush()
+    # def putc(address, value):
+        # if not started:
+        #     return
+        # try:
+        #     if value==0x08:
+        #         sys.stdout.write(chr(value))
+        #         sys.stdout.write(' ')
+        #     sys.stdout.write(chr(value))
+        # except UnicodeEncodeError: # Python 3
+        #     sys.stdout.write("?")
+        # sys.stdout.flush()
 
     def getc(address):
-        if queue.empty():
-            return 0
-        else:
-            c=queue.get()
-            return c
+        while queue.empty():
+            pass
+        c = queue.get()
+        print(chr(c), end="")
+        return c
 
     def getByte(address):
         return mpu.memory[address]
@@ -69,13 +111,66 @@ def cpuThread(ch, queue, emu_queue):
     def getWord(address):
         return mpu.memory[address] + 256*mpu.memory[address+1]
 
+    def getCountedStr(addr,l=None):
+        if l==None:
+            l = getByte(addr) # length byte
+        ba = mpu.memory[addr+1:addr+l+1]
+        return "".join(map(chr,ba))
+
+    def do_0BR():
+        if getWord( 2 + mpu.x ) != 0x0000:
+            return # not taking the branch, do nothing here
+        addr_jump_to = getWord( getWord( addr_IP ) )
+        if addr_jump_to == addr_numberError:
+            print("FATAL: Word not found:",last_lookedup_word)
+            print("COMPILATION ABORTED")
+            emu_queue.put(CPU_EXIT_ERROR)
+            quit()
+
+    def do_FIND():
+        # Simulates FIND in python and bypasses our FORTH's FIND.
+        # Saves a lot of cycles
+        global last_lookedup_word
+
+        addr = getWord( 4 + mpu.x )
+        l = getWord( 2 + mpu.x )
+        word = "".join(map(chr, mpu.memory[addr:addr+l] ))
+
+        last_lookedup_word = word
+
+        if args.dfo:
+            # user forced Disable FIND Offloading
+            return
+
+        header = getWord(addr_LATEST)
+        while True:
+            l = getByte(header+2) & 0x1F
+            str = getCountedStr(header+2, l)
+
+            if str == word:
+                # put  header addr in 5,X, 4,X (NOS)
+                mpu.memory[4+mpu.x] = header & 0xFF
+                mpu.memory[5+mpu.x] = header >> 8
+                mpu.x = mpu.x + 2   # DROP
+                mpu.pc = addr_NEXT  #  JMP NEXT
+                break
+
+            header = getWord(header)
+
+            if header == 0:
+                mpu.memory[4+mpu.x] = 0
+                mpu.memory[5+mpu.x] = 0
+                mpu.x = mpu.x + 2
+                mpu.pc = addr_NEXT
+                break
+
     mpu = CMOS65C02()
     mpu.memory = 0x10000 * [0xEA]
 
     addrWidth = mpu.ADDR_WIDTH
 
     m = ObservableMemory(subject=mpu.memory, addrWidth=addrWidth)
-    m.subscribe_to_write([putc_addr], putc)
+    # m.subscribe_to_write([putc_addr], putc)
     m.subscribe_to_read([getc_addr], getc)
     mpu.memory = m
 
@@ -93,6 +188,14 @@ def cpuThread(ch, queue, emu_queue):
     started = True
 
     while True:
+        if mpu.pc == addr_do_FIND:
+            # fast FIND offloaded in python
+            do_FIND()
+        elif mpu.pc == addr_do_0BR:
+            # check if we've reached the numberError condition
+            # means word not found in interpreter --> abort compilation
+            do_0BR()
+
         if getByte(0x0000) == 0x01:
             # Means the end of compilation, exit the CPU loop
             break
@@ -144,7 +247,7 @@ def cpuThread(ch, queue, emu_queue):
     print(mpu.processorCycles, "clock cycles")
 
     # Signal main thread it's the end
-    emu_queue.put(1)
+    emu_queue.put(CPU_EXIT_OK)
 
 # We start the "computer"
 t=threading.Thread( target=cpuThread, args=("", queue, emu_queue))
@@ -170,3 +273,5 @@ while True:
     if not emu_queue.empty():
         print("CPU signaled end of compilation!")
         break
+
+quit(emu_queue.get())
