0b324b95370b3272fb9b2677f006bca96525877e
Alexandre Dumont <adumont@gmail.com>
2022-10-01 19:56:04 +0200

    Forth: autogenerated version

---
+++ forth/version.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+REPO=$( git config --get remote.origin.url | cut -d: -f2 | sed -e 's/\.git//' )
+BRANCH=$( git symbolic-ref --short HEAD )
+SHA=$( git rev-parse --short HEAD )
+DATE=$( git log -1 --format=%cd --date=format:"%y%m" )
+YEAR=$( git log -1 --format=%cd --date=format:"%Y" )
+#DATE=$( date +%y%m )
+NUMB=$( git log --oneline | wc -l )
+
+#echo "$REPO($BRANCH):$SHA-$DATE-$NUMB" > .version
+
+#VERSION="$DATE-$NUMB"
+VERSION="$DATE"
+
+if [ "$BRANCH" != "main" ]
+then
+    VERSION="$VERSION($BRANCH)"
+fi
+VERSION="$VERSION-$SHA"
+
+echo "VERS_STR: CString {\"ALEXFORTH $VERSION\", \$0A, \$0D, \"(c) 2021-$YEAR Alex Dumont\", \$0A, \$0D}"
