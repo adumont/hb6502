5b95a5cde4e8be2ad43f5f4ffe82f2d3fc0b84fe
Alexandre Dumont <adumont@gmail.com>
2024-02-05 23:50:59 +0100

    ski words

--- forth/bootstrap.f
+++ forth/bootstrap.f
@@ -232,8 +232,128 @@ _BP BP !
 
 : .S DEPTH DUP IF 1+ DUP 1 DO DUP I - PICK . LOOP CR THEN DROP ;
 
+\ \\\\\\\\\\\\\\\\\\\\\\\ SKI CALCULUS EXTENSION
+
+\ SKI Calculus in AlexForth
+\ Tested in AlexForth for 6502
+\ (C) 2023 Alexandre Dumont <adumont@gmail.com>
+\ SPDX-License-Identifier: GPL-3.0-only
+
+\ ENTER, places a empty header in the dictionary
+\ $4C is 6502's JMP
+: ENTER, ( -- ) 4C C, COMPILE COLON ;
+
+\ Create a Function (or combinator)
+\ Combinators are Higher Order Functions, meaning
+\ they take a function as an Argument and return a function
+\ which we will eventually apply later using ")"
+: :FUNC ( "name" -- ) CREATE ENTER, ] ;
+
+
+\ Application operator
+: )   ( xt -- xt ) EXEC  ; \ Apply <=> "Application"
+
+\ Syntactic sugar definitions
+: ))  ( xt -- xt ) ) )   ;
+: ))) ( xt -- xt ) ) ) ) ;
+
+
+\ Here we redefine I,
+\ no problem we can always use R> in DO LOOPs...
+
+\ Identity Combinator
+\ Ix=x    λx.x
+:FUNC I ;
+
+\ Constant Combinator, aka Kestrel
+\ Kxy=x   λxy.x
+:FUNC K
+  HERE \ leaves the XT of the :NONAME word on the stack
+  \ now we compile the :NONAME word
+  ENTER,
+  COMPILE DROP     \ Drop X
+  COMPILE LIT      \ Push Y onto the stack
+  SWAP \ put Y back on TOS
+  ,  \ store Y into the definition
+  COMPILE EXIT
+;
+
+\ Kite Combinator
+\ KIxy=y    λxy.y
+I K )   CONSTANT   KI
+
+\ Cardinal combinator
+\ λfab.fba  Cfab=fba
+:FUNC C ( f -- CF )
+  HERE
+  ENTER,
+  COMPILE HERE
+  COMPILE ENTER,
+  COMPILE COMPILE COMPILE LIT
+  COMPILE SWAP
+  COMPILE ,   \ stores a
+  COMPILE COMPILE COMPILE SWAP
+  COMPILE COMPILE COMPILE LIT
+  COMPILE LIT
+  SWAP
+  ,
+  COMPILE ,
+  COMPILE COMPILE COMPILE ))
+  COMPILE COMPILE COMPILE EXIT
+  COMPILE EXIT
+;
+
+\ S combinator
+\ λxyz.xz(yz)  Sxyz = xz(yz)
+:FUNC S
+  HERE
+  ENTER,
+  COMPILE HERE
+  COMPILE ENTER,
+  COMPILE SWAP
+  COMPILE COMPILE COMPILE DUP
+  COMPILE COMPILE COMPILE LIT \ y
+  COMPILE , \ y
+  COMPILE COMPILE COMPILE )
+  COMPILE COMPILE COMPILE SWAP
+  COMPILE COMPILE COMPILE LIT
+  COMPILE LIT \ x
+  SWAP
+  , \ store x
+  COMPILE ,
+  COMPILE COMPILE COMPILE ))
+  COMPILE COMPILE COMPILE EXIT
+  COMPILE EXIT
+;
+
+\ Hack: we define those two functions so we can check results of boolean operations
+:FUNC .T .( TRUE )  ;
+:FUNC .F .( FALSE ) ;
+
+: BOOL .F .T ;
+
+\ BOOLEANS
+K  CONSTANT T    \ TRUE  λxy.x
+KI CONSTANT F    \ FALSE λxy.y
+
+\ Test with:
+\ .F .T K  )))  --> TRUE
+\ .F .T KI )))  --> FALSE
+
+\ We define the INCR function so we
+\ can check results of church numerals operations
+:FUNC INCR 1+ ;
+: CN 0 INCR ;
+
 >RAM
 MARKER
+
+\ Garbage collection
+DEFER _FORGET
+:NONAME ; IS _FORGET \ NOP
+: GC _FORGET MARKER ; GC
+' FORGET IS _FORGET
+
 
 \ DON'T REMOVE THIS CODE block
 >ROM
